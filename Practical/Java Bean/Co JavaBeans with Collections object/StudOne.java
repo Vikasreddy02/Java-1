5dec37e491954a4b4bec9f2b85eeec20d182f7f7
TejasKute96 <87482740+TejasKute96@users.noreply.github.com>
2022-05-27 12:16:38 +0530

    Add files via upload

---
+++ Practical/Java Bean/Co JavaBeans with Collections object/StudOne.java
@@ -0,0 +1,21 @@
+package corejava;
+import java.util.*;
+	class StudOne {
+		public static void main(String[] args) {
+			Stud student = new Stud();
+			String[] subjects = { "English", "Science", "Computer" };
+			
+			// setting bean values
+			student.setId(183143);
+			student.setName("Karthik");
+			student.setSubjects(subjects);
+			
+			// getting bean value
+			System.out.println("Student Id : " + student.getId());
+			System.out.println("Student name : " + student.getName());
+			String[] subjectArray = student.getSubjects();
+			for (int i = 0; i < subjectArray.length; i++) {
+				System.out.println("Student subject " + (i + 1) + " : " + subjectArray[i]);
+			}
+		}
+	}
